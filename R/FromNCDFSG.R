eb46518d60d75dc8f042e3e22143eaeac1d3da34
David Blodgett <dblodgett@usgs.gov>
2016-11-22 18:19:34 -0600

    reindent

--- R/FromNCDFSG.R
+++ R/FromNCDFSG.R
@@ -34,64 +34,64 @@ FromNCDFSG = function(nc_file) {
   if(point) {
 
   } else {
-  node_data <- strsplit(ncatt_get(nc, coord_index_var, attname = "geom_coordinates")$value, " ")[[1]]
+    node_data <- strsplit(ncatt_get(nc, coord_index_var, attname = "geom_coordinates")$value, " ")[[1]]
 
-  stop_inds <- ncvar_get(nc, coord_index_stop_var)
-  instance_names <- ncvar_get(nc, instance_id)
-  start_ind <- 1
-  Srl <- list()
-  ragged_ind_var<-ncvar_get(nc, coord_index_var)
+    stop_inds <- ncvar_get(nc, coord_index_stop_var)
+    instance_names <- ncvar_get(nc, instance_id)
+    start_ind <- 1
+    Srl <- list()
+    ragged_ind_var<-ncvar_get(nc, coord_index_var)
 
-  for(geom in 1:length(stop_inds)) {
-    stop_ind <- stop_inds[geom]
-    ragged_inds <- ragged_ind_var[start_ind:stop_ind]
-    breaks <- sort(c(which(ragged_inds == hole_break_val), which(ragged_inds == multi_break_val)))
-    multi_hole <- rep(FALSE, length(breaks))
-    multi_hole[which(ragged_inds[breaks] == hole_break_val)] <- TRUE
-    srl <- list()
-    coords_start <- 1
-    hole<-FALSE # First is always a polygon, not a hole?
-    if(length(breaks) > 0) {
-      for(part in 1:length(breaks)) {
-        coords_count <- ragged_inds[breaks[part]-1] - coords_start + 1
-        if(poly) {
-          srl <- append(srl, getPolysrl(nc, node_data, coords_start, coords_count, hole))
-        } else if(line) {
-          srl <- append(srl, getLinesrl(nc, node_data, coords_start, coords_count))
+    for(geom in 1:length(stop_inds)) {
+      stop_ind <- stop_inds[geom]
+      ragged_inds <- ragged_ind_var[start_ind:stop_ind]
+      breaks <- sort(c(which(ragged_inds == hole_break_val), which(ragged_inds == multi_break_val)))
+      multi_hole <- rep(FALSE, length(breaks))
+      multi_hole[which(ragged_inds[breaks] == hole_break_val)] <- TRUE
+      srl <- list()
+      coords_start <- 1
+      hole<-FALSE # First is always a polygon, not a hole?
+      if(length(breaks) > 0) {
+        for(part in 1:length(breaks)) {
+          coords_count <- ragged_inds[breaks[part]-1] - coords_start + 1
+          if(poly) {
+            srl <- append(srl, getPolysrl(nc, node_data, coords_start, coords_count, hole))
+          } else if(line) {
+            srl <- append(srl, getLinesrl(nc, node_data, coords_start, coords_count))
+          }
+          coords_start <- ragged_inds[breaks[part]+1] # Increments to the next position where there is an index.
+          hole<-multi_hole[part] # Indicates that a hole is coming next.
         }
-        coords_start <- ragged_inds[breaks[part]+1] # Increments to the next position where there is an index.
-        hole<-multi_hole[part] # Indicates that a hole is coming next.
+      }
+      coords_count <- ragged_inds[stop_ind]-coords_start+1
+      if(poly) { # This could be refactored along with the two functions declared below (getPolysrl and getLinesrl)
+        srl <- append(srl, getPolysrl(nc, node_data, coords_start, coords_count, hole))
+        Srl <- append(Srl, Polygons(srl, as.character(geom)))
+      }  else if(line) {
+        srl <- append(srl, getLinesrl(nc, node_data, coords_start, coords_count))
+        Srl <- append(Srl, Lines(srl, as.character(geom)))
       }
     }
-    coords_count <- ragged_inds[stop_ind]-coords_start+1
-    if(poly) { # This could be refactored along with the two functions declared below (getPolysrl and getLinesrl)
-      srl <- append(srl, getPolysrl(nc, node_data, coords_start, coords_count, hole))
-      Srl <- append(Srl, Polygons(srl, as.character(geom)))
-    }  else if(line) {
-      srl <- append(srl, getLinesrl(nc, node_data, coords_start, coords_count))
-      Srl <- append(Srl, Lines(srl, as.character(geom)))
+
+    dataFrame <- as.data.frame(list(id=1:nc$var[coord_index_stop_var][[1]]$dim[[1]]$len))
+
+    for(var in nc$var) {
+      if(var$ndims==1 && grepl(var$dim[[1]]$name,paste0("^",instance_id,"$")) &&
+         !grepl(var$name, paste0("^",coord_index_stop_var,"$"))) {
+        dataFrame[var$name] <- ncvar_get(nc, var$name)
+      } else if(grepl(var$prec, paste0("^char$")) &&
+                (grepl(var$dim[[1]]$name,paste0("^",instance_id,"$")) ||
+                 grepl(var$dim[[2]]$name,paste0("^",instance_id,"$"))))
+        dataFrame[var$name] <- ncvar_get(nc, var$name)
+    }
+    if(poly) {
+      SPGeom <- SpatialPolygonsDataFrame(SpatialPolygons(Srl, proj4string = CRS("+proj=longlat +datum=WGS84")),
+                                         dataFrame, match.ID = FALSE)
+    } else if(line) {
+      SPGeom <- SpatialLinesDataFrame(SpatialLines(Srl, proj4string = CRS("+proj=longlat +datum=WGS84")),
+                                      dataFrame, match.ID = FALSE)
     }
   }
-
-  dataFrame <- as.data.frame(list(id=1:nc$var[coord_index_stop_var][[1]]$dim[[1]]$len))
-
-  for(var in nc$var) {
-    if(var$ndims==1 && grepl(var$dim[[1]]$name,paste0("^",instance_id,"$")) &&
-       !grepl(var$name, paste0("^",coord_index_stop_var,"$"))) {
-      dataFrame[var$name] <- ncvar_get(nc, var$name)
-    } else if(grepl(var$prec, paste0("^char$")) &&
-              (grepl(var$dim[[1]]$name,paste0("^",instance_id,"$")) ||
-               grepl(var$dim[[2]]$name,paste0("^",instance_id,"$"))))
-      dataFrame[var$name] <- ncvar_get(nc, var$name)
-  }
-  if(poly) {
-    SPGeom <- SpatialPolygonsDataFrame(SpatialPolygons(Srl, proj4string = CRS("+proj=longlat +datum=WGS84")),
-                                       dataFrame, match.ID = FALSE)
-  } else if(line) {
-    SPGeom <- SpatialLinesDataFrame(SpatialLines(Srl, proj4string = CRS("+proj=longlat +datum=WGS84")),
-                                    dataFrame, match.ID = FALSE)
-  }
-}
   return(SPGeom)
 }
 
@@ -143,29 +143,29 @@ checkNCDF <- function(nc) {
   geom_type <- try(ncatt_get(nc, coord_index_var, attname = "geom_type")$value)
 
   if(grepl("multipolygon", geom_type) || grepl("multiline", geom_type)) {
-  # Look for 'geom_coordinates' that match variable names.
-  coord_index_var<-list()
-  for(var in c(names(nc$var), names(nc$dim))) { # need to come back to handle coordinate variables named the same as the dimension.
-    coord_index_var<-append(coord_index_var, findVarByAtt(nc, "geom_coordinates", var, strict=FALSE))
-  }
-  coord_index_var<-unique(coord_index_var)[[1]]
+    # Look for 'geom_coordinates' that match variable names.
+    coord_index_var<-list()
+    for(var in c(names(nc$var), names(nc$dim))) { # need to come back to handle coordinate variables named the same as the dimension.
+      coord_index_var<-append(coord_index_var, findVarByAtt(nc, "geom_coordinates", var, strict=FALSE))
+    }
+    coord_index_var<-unique(coord_index_var)[[1]]
 
-  if(length(coord_index_var)>1) {stop('only one geom_coordinates index is supported, this file has more than one.')}
+    if(length(coord_index_var)>1) {stop('only one geom_coordinates index is supported, this file has more than one.')}
 
-  if(length(coord_index_var)==0) {
-    stop('No geometry coordinates were found in the file but required for geometry.') }
+    if(length(coord_index_var)==0) {
+      stop('No geometry coordinates were found in the file but required for geometry.') }
 
-  coord_index_stop_var<-list()
-  for(dimen in c(names(nc$dim))) {
-    coord_index_stop_var <- append(coord_index_stop_var, findVarByAtt(nc, "contiguous_ragged_dimension", dimen))
-  }
-  coord_index_stop_var <- unique(coord_index_stop_var)[[1]]
+    coord_index_stop_var<-list()
+    for(dimen in c(names(nc$dim))) {
+      coord_index_stop_var <- append(coord_index_stop_var, findVarByAtt(nc, "contiguous_ragged_dimension", dimen))
+    }
+    coord_index_stop_var <- unique(coord_index_stop_var)[[1]]
 
-  if(length(coord_index_stop_var)>1) {stop('only one contiquous ragged dimension index is supported, this file has more than one.')}
+    if(length(coord_index_stop_var)>1) {stop('only one contiquous ragged dimension index is supported, this file has more than one.')}
 
-  try(multi_break_val <- ncatt_get(nc, coord_index_var, 'multipart_break_value')$value)
-  try(hole_break_val <- ncatt_get(nc, coord_index_var, 'hole_break_value')$value)
-  # Could also implement outer_ring_order and closure convention.
+    try(multi_break_val <- ncatt_get(nc, coord_index_var, 'multipart_break_value')$value)
+    try(hole_break_val <- ncatt_get(nc, coord_index_var, 'hole_break_value')$value)
+    # Could also implement outer_ring_order and closure convention.
   }
 
   return(list(instance_id=instance_id,
