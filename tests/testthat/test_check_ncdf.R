212d175207629f4199d9a189a4a3478ee3a14580
David Blodgett <dblodgett@usgs.gov>
2017-03-15 08:30:41 -0500

    tests

--- tests/testthat/test_check_ncdf.R
+++ tests/testthat/test_check_ncdf.R
@@ -65,47 +65,48 @@ test_that("basic point works", {
   expect_equal(checkVals$instanceDim, pkg.env$instance_dim_name)
 })
 
-# test_that("errors", {
-#   multipointData <- readRDS("data/pointData.rds")
-#   nc_file <- ToNCDFSG(nc_file=tempfile(), geomData = multipointData)
-#   nc <- nc_open(nc_file, write = TRUE)
-#   ncatt_put(nc, nc$var$instance_name, "cf_role", "garbage")
-#   ncatt_put(nc, nc$var$instance_name, "standard_name", "garbage")
-#   expect_error(checkNCDF(nc), 'A timeseries id variable was not found in the file.')
-#
-#   nc_file <- ToNCDFSG(nc_file=tempfile(), geomData = multipointData)
-#   nc <- nc_open(nc_file, write = TRUE)
-#   ncatt_put(nc, nc$var$lat, "cf_role", "timeseries_id")
-#   expect_error(checkNCDF(nc), 'multiple timeseries id variables were found.')
-#
-#   nc_file <- ToNCDFSG(nc_file=tempfile(), geomData = multipointData)
-#   nc <- nc_open(nc_file, write = TRUE)
-#   ncatt_put(nc, 0,"Conventions", "garbage")
-#   expect_warning(checkNCDF(nc), 'File does not advertise CF conventions, unexpected behavior may result.')
-#
-#   lineData <- readRDS("data/multiLineData.rds")
-#   nc_file <- ToNCDFSG(nc_file=tempfile(), geomData = lineData)
-#   nc<-nc_open(nc_file, write = TRUE)
-#   ncatt_put(nc, nc$var$instance_name, "geom_coordinates", "x y")
-#   nc_close(nc)
-#   nc<-nc_open(nc_file, write = TRUE)
-#   expect_error(checkNCDF(nc), "only one geom_coordinates index is supported, this file has more than one.")
-#
-#   lineData <- readRDS("data/multiLineData.rds")
-#   nc_file <- ToNCDFSG(nc_file=tempfile(), geomData = lineData)
-#   nc<-nc_open(nc_file, write = TRUE)
-#   ncatt_put(nc, nc$var$x, "contiguous_ragged_dimension", "coordinate_index")
-#   nc_close(nc)
-#   nc<-nc_open(nc_file, write = TRUE)
-#   expect_error(checkNCDF(nc), "only one contiquous ragged dimension index is supported, this file has more than one.")
-#
-#   multipointData <- readRDS("data/pointData.rds")
-#   nc_file <- ToNCDFSG(nc_file=tempfile(), geomData = multipointData)
-#   nc <- nc_open(nc_file, write = TRUE)
-#   ncatt_put(nc, nc$var$lat, "standard_name", "garbage")
-#   expect_warning(checkNCDF(nc), "instance dimension is being inferred based on an assumption of dimension order of the character instance_id and may not be correct.")
-#
-#   nc <- nc_open("data/borked_featureType.nc")
-#
-#   expect_warning(checkNCDF(nc), "File does not have a featureType declaration, unexpected behavior may result.")
-# })
+test_that("errors", {
+  multipointData <- readRDS("data/pointData.rds")
+  nc_file <- ToNCDFSG(nc_file=tempfile(), geomData = multipointData)
+  nc <- nc_open(nc_file, write = TRUE)
+  ncatt_put(nc, nc$var$instance_name, pkg.env$geom_type_attr_name, "garbage")
+  nc_close(nc)
+  nc<-nc_open(nc_file, write = TRUE)
+  expect_error(checkNCDF(nc), "only one geometry container per file supported")
+
+  # nc_file <- ToNCDFSG(nc_file=tempfile(), geomData = multipointData)
+  # nc <- nc_open(nc_file, write = TRUE)
+  # ncatt_put(nc, nc$var$lat, "cf_role", "timeseries_id")
+  # expect_error(checkNCDF(nc), 'multiple timeseries id variables were found.')
+
+  nc_file <- ToNCDFSG(nc_file=tempfile(), geomData = multipointData)
+  nc <- nc_open(nc_file, write = TRUE)
+  ncatt_put(nc, 0,"Conventions", "garbage")
+  expect_warning(checkNCDF(nc), 'File does not advertise CF conventions, unexpected behavior may result.')
+
+  # lineData <- readRDS("data/multiLineData.rds")
+  # nc_file <- ToNCDFSG(nc_file=tempfile(), geomData = lineData)
+  # nc<-nc_open(nc_file, write = TRUE)
+  # ncatt_put(nc, nc$var$instance_name, "node_coordinates", "x y")
+  # nc_close(nc)
+  # nc<-nc_open(nc_file, write = TRUE)
+  # expect_error(checkNCDF(nc), "only one node_coordinates index is supported, this file has more than one.")
+  #
+  # lineData <- readRDS("data/multiLineData.rds")
+  # nc_file <- ToNCDFSG(nc_file=tempfile(), geomData = lineData)
+  # nc<-nc_open(nc_file, write = TRUE)
+  # ncatt_put(nc, nc$var$x, "contiguous_ragged_dimension", "coordinate_index")
+  # nc_close(nc)
+  # nc<-nc_open(nc_file, write = TRUE)
+  # expect_error(checkNCDF(nc), "only one contiquous ragged dimension index is supported, this file has more than one.")
+  #
+  # multipointData <- readRDS("data/pointData.rds")
+  # nc_file <- ToNCDFSG(nc_file=tempfile(), geomData = multipointData)
+  # nc <- nc_open(nc_file, write = TRUE)
+  # ncatt_put(nc, nc$var$lat, "standard_name", "garbage")
+  # expect_warning(checkNCDF(nc), "instance dimension is being inferred based on an assumption of dimension order of the character instance_id and may not be correct.")
+  #
+  # nc <- nc_open("data/borked_featureType.nc")
+  #
+  # expect_warning(checkNCDF(nc), "File does not have a featureType declaration, unexpected behavior may result.")
+})
